a76f2a56b16a006aed1d6786d6d1b94b7ea3f3bb
Nguyen Xuan Manh Cuong <134146370+cuongnxm96@users.noreply.github.com>
2025-06-20 17:01:31 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+// using System.Reflection;
+using System.Reflection;
+using DbUp;
+
+string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "";
+string tablesCreateDirectory = Path.Combine(path, @"Scripts\Tables\Create");
+var upgradeEngine = DeployChanges.To
+    .SqlDatabase("Server=103.110.85.206; Database=db_FinancialManagement; User Id=user_fm; Password=fmdb@12345; TrustServerCertificate=True;")
+    .WithScriptsFromFileSystem(tablesCreateDirectory)
+    .LogToConsole()
+    .Build();
+var result = upgradeEngine.PerformUpgrade();
+Console.WriteLine(tablesCreateDirectory);   
